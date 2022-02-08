539a7007297507f6b284504e2aa36f76845e04ae
Huilin Chen <chen0771@algonquinlive.com>
2022-02-08 17:49:13 -0500

    add assig-1

---
+++ assig-1/theme_export__huilinchen-mtm6303-myshopify-com-copy-of-debut__08FEB2022-0541pm/assets/custom-popup.js
@@ -0,0 +1,6 @@
+// alert("This is an alert")
+window.addEventListener('load'(event) => {
+var x = document.getElementsByclassName(".custom-popup-container");
+console.log(x);
+x.style.display = "block";
+}
